f3f18086f8ea2f342bbcd50a2de92179188b7453
Jinah <monsatfri@gmail.com>
2024-02-13 01:55:33 +0900

    feat: Header 컴포넌트 내 알림 모달 추가

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,12 +2,60 @@ import { useNavigate } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 import chat from '../../assets/ic-chat.svg';
 import './header.scss';
+import { useState, useRef, useEffect } from 'react';
 
 const Header = () => {
   const navigate = useNavigate();
+  const [showChatBox, setShowChatBox] = useState<boolean>(false);
+  const chatBoxRef = useRef<HTMLDivElement>(null);
+  const lastScrollTopRef = useRef<number>(0);
 
-  const handlePop = () => {
-    alert('채팅 기능은 준비 중입니다.');
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        chatBoxRef.current &&
+        !chatBoxRef.current.contains(event.target as Node) &&
+        !isDescendant(chatBoxRef.current, event.target as Node) &&
+        !(event.target as HTMLElement).classList.contains('header-chat')
+      ) {
+        setShowChatBox(false);
+      }
+    };
+
+    const handleScroll = () => {
+      if (chatBoxRef.current) {
+        const scrollTop = chatBoxRef.current.scrollTop;
+        if (scrollTop > lastScrollTopRef.current) {
+          setShowChatBox(false);
+        }
+        lastScrollTopRef.current = scrollTop;
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('scroll', handleScroll);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const isDescendant = (parent: Node, child: Node) => {
+    let node = child.parentNode;
+
+    while (node !== null) {
+      if (node === parent) {
+        return true;
+      }
+      node = node.parentNode;
+    }
+
+    return false;
+  };
+
+  const handleChatClick = () => {
+    setShowChatBox(!showChatBox);
   };
 
   return (
@@ -25,9 +73,22 @@ const Header = () => {
           src={chat}
           alt="chat"
           className="header-chat"
-          onClick={handlePop}
+          onClick={handleChatClick}
         />
-        <div className="header-chat-box"></div>
+        <div
+          className={`header-chat-box scrollBar ${showChatBox ? 'active' : ''}`}
+          ref={chatBoxRef}
+        >
+          <div className="header-chat-title">
+            <span>🔔알림이 도착했습니다.</span>
+          </div>
+          {Array.from({ length: 15 }).map((_, index) => (
+            <div key={index} className="header-chat-content">
+              <span>이곳에 알림 내용이 표시됩니다.</span>
+              <hr />
+            </div>
+          ))}
+        </div>
         <button
           className="header-btn-login"
           onClick={() => {
